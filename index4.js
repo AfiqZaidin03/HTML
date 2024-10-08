97f7563f9559abb48d926fc4e7571f9a18bc9b4a
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-10-08 11:57:54 +0800

    [21] Type function calls

--- index4.js
+++ index4.js
@@ -1,9 +1,17 @@
-
+// Performing a task
 // name is parameter
 function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName);
 }
 
+// Calculating a value
+function square(number) {
+    return number * number;
+}
 
 // Afiq is argument 
 greet('Afiq', 'Zaidin');
+
+let number = square(2);
+console.log(number);
+console.log(square(3));
