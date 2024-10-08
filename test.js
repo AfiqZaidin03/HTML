b225874c6904b9ff6d9e0e56f5eb8a0d6c2a58eb
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-10-08 15:02:00 +0800

    [24] Object

--- test.js
+++ test.js
@@ -6,6 +6,21 @@ if (true) {
 }
 
 // New ternary operators
-let age = 20;
-let name = age > 10 ? "Afiq" : "Muhd";
+// let age = 20;
+// let name = age > 10 ? "Afiq" : "Muhd";
+
+// Object
+const person = {
+    name: "Afiq",
+    age: 20,
+    isMarried: false,
+}
+
+// Old object call
+const nama = person.name;
+const umur = person.age;
+const isKahwin= person.isMarried;
+
+// New object call
+const {name, age, isMarried} = person;
 
