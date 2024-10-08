4f1b731e26a070e4cf48b73c414f0a9c39420be9
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-10-08 15:02:50 +0800

    [25] Change name

---
+++ operators.js
@@ -0,0 +1,26 @@
+// Old ternary operators
+if (true) {
+
+} else {
+
+}
+
+// New ternary operators
+// let age = 20;
+// let name = age > 10 ? "Afiq" : "Muhd";
+
+// Object
+const person = {
+    name: "Afiq",
+    age: 20,
+    isMarried: false,
+}
+
+// Old object call
+const nama = person.name;
+const umur = person.age;
+const isKahwin= person.isMarried;
+
+// New object call
+const {name, age, isMarried} = person;
+
