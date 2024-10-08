571767be1b64dbef99807c9167c2937970200900
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-10-08 15:15:31 +0800

    [27] Array object

---
+++ object.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Afiq",
+    age: 20,
+    isMarried: false,
+}
+
+const person2 = {... person, name: "Naruto"}
+
+// Array
+const names = ["Kakashi", "Sakura", "Rock Lee"];
+const names2 = [...names, "Minato"];
