0e49a2422fe1429a4320751301e666e96292e210
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-10-08 11:46:41 +0800

    [18] Object practise

--- index2.js
+++ index2.js
@@ -3,3 +3,20 @@ let age = 20; // Number Literal
 let isApproved = true; // Boolean Literal
 let firstName = undefined;
 let lastName = null;
+
+// Object literal
+let person = {
+    name: 'Afiq',
+    age: 20,
+};
+
+// Dot Notation
+person.name = 'John';
+
+// Bracket Notation
+person['name'] = 'Sasuke'
+
+let selection = 'name';
+person[selection] = 'Itachi'
+
+console.log(person.name);
